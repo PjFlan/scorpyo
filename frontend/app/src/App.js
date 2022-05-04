aaa1e45f97d6550b40d0b0afe5ee82462d344476
Padraic Flanagan <padraicjflan@gmail.com>
2022-05-04 19:33:50 +0100

    cleanup: delete react defaults

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
     useEffect(() => {
       const ws = new WebSocket('ws://127.0.0.1:13254')
+
       ws.onmessage = function (event) {
           const json = JSON.parse(event.data);
           try {
@@ -20,18 +20,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Scorpyo!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
